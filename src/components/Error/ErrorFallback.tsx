f7f073700ba000c5f2e9b8afbc2f0656193e5357
ilia makarov <ilia.makarov@me.com>
2024-03-13 17:03:09 +0200

    More detailed error handling

--- src/components/Error/ErrorFallback.tsx
+++ src/components/Error/ErrorFallback.tsx
@@ -1,4 +1,5 @@
 import { get } from "lodash";
+import { match } from "ts-pattern";
 import { Stack } from "@deskpro/deskpro-ui";
 import { DEFAULT_ERROR } from "../../constants";
 import { JamfError } from "../../services/jamf";
@@ -14,12 +15,17 @@ const ErrorFallback: FC<Props> = ({ error }) => {
   let message = DEFAULT_ERROR;
   let consoleMessage;
 
-
   if (error instanceof JamfError) {
-    message = get(error, ["data", "error_description"])
-      || get(error, ["data", "error"])
-      || get(error, ["data", "errors", 0, "description"])
-      || DEFAULT_ERROR;
+    consoleMessage = error;
+
+    message = match(get(error, ["data", "httpStatus"]))
+      .with(401, () => "Authentication failed. Verify the credentials being used for the request.")
+      .with(403, () => "Invalid permissions. Verify the account being used has the proper permissions for the resource you are trying to access.")
+      .otherwise(() => get(error, ["data", "error_description"])
+        || get(error, ["data", "error"])
+        || get(error, ["data", "errors", 0, "description"])
+        || DEFAULT_ERROR
+      )
   }
 
   // eslint-disable-next-line no-console
